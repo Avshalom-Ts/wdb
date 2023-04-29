2018d688de278058bffe0e1c096c43a7ec16d157
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-04-29 18:54:09 +0000

    CRUD working

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
-app = express();
-
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
 
 const todoRoutes = require('./routes/todos');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req,res){
   res.send("<h1>Hello from the root route</h1>");
@@ -12,7 +14,6 @@ app.get('/', function(req,res){
 
 app.use('/api/todos', todoRoutes);
 
-const port = 8080
 
 app.listen(port, function(){
     console.log(`APP IS RUNNING ON PORT http://localhost:${port}`);
